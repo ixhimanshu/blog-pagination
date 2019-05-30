1f1f6b8d59055a45e0946d2a041cce1c16c0d53f
ixhimanshu <himanshu011196@gmail.com>
2019-05-31 03:31:50 +0530

    pagination updates

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// var count = 5;
+// var button = document.querySelector(".next");
 
+
+var newselectedvalue = 6;
 
 
 document.querySelector(".pagination").addEventListener("click", function (e) {
-    // document.querySelector(".link").classList.add("active");
+
+   
     
-    var newselectedvalue = e.path[0].innerHTML;
-    console.log(newselectedvalue);
+    if( e.path[0].innerHTML == ""){
+        newselectedvalue = 1;
+        return newselectedvalue;
+    } else{
+        e.path[0].className = "active";
+        var newselectedvalue = e.path[0].innerHTML;
+    }
+    
+
+    
+
+    console.log(e.path);
+
     resetpost();
 
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,7 +30,7 @@ document.querySelector(".pagination").addEventListener("click", function (e) {
 
     var mappost = results.map( (abc) => {
 
-    if( abc.id <= newselectedvalue*window.itemstoshow && abc.id >= newselectedvalue*window.itemstoshow - window.itemstoshow) {
+    if( abc.id < newselectedvalue*window.itemstoshow && abc.id >= newselectedvalue*window.itemstoshow - window.itemstoshow) {
     document.getElementById("posts").innerHTML += `<div class="card-ui"> 
   
     <div class="title">  ${abc.id}. ${abc.title} </div>  
@@ -36,10 +52,10 @@ document.querySelector(".pagination").addEventListener("click", function (e) {
 
 
 function newdata(value){
-      window.itemstoshow = value;
 
      resetpost();
-   
+
+     window.itemstoshow = value;
 
      fetch('https://jsonplaceholder.typicode.com/posts')
      .then( (res) => res.json())
@@ -64,13 +80,53 @@ function newdata(value){
      .catch( (err) => console.log(err))
 }
 
-// console.log(itemstoshow);
+newdata(5);
+
 
 
 function resetpost(){
     document.getElementById("posts").innerHTML = '';
 }
 
+var newcounter = 1;
+
+function next(){
+    
+    newcounter++
+    newselectedvalue++
+
+    document.getElementById("pagi").innerHTML = `
+    <a class="prev" href="#" onclick="previus()"></a>
+    <a class="link" href="#">${newcounter}</a>
+    <a class="link" href="#">${newcounter + 1}</a>
+    <a href="#"  class="link">${newcounter + 2}</a>
+
+    <a href="#" class=" next" onclick="next()"></a>
+    `;
+
+    console.log(newcounter)
+}
+
+function previus(){
+    
+    
+
+    if ( newcounter >= 4 ){
+        newcounter--
+        document.getElementById("pagi").innerHTML = `
+        <a class="prev" href="#" onclick="previus()"></a>
+        <a class="link" href="#">${newcounter - 2}</a>
+        <a href="#"  class="link">${newcounter - 1}</a>
+        <a href="#" class="link">${newcounter}</a>
+    
+        <a href="#" class=" next" onclick="next()"></a>
+        `;
+    }
+
+    console.log(newcounter)
+
+   
+}
 
 
     
@@ -81,6 +137,3 @@ function resetpost(){
 
 
 
-
-
-console.log(window.itemstoshow);
